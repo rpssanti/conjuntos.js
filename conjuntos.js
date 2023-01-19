0000ebde4822b6ba49821b5875ae25e8daf4d702
Santi <121914283+rpssanti@users.noreply.github.com>
2023-01-20 00:47:17 +0100

    Add files via upload

---
+++ conjuntos.js
@@ -0,0 +1,20 @@
+//Ejercicio leccion 7 
+
+
+
+const miSet = new Set (["Irene", "Santy", "Mikel"])
+
+console.log(miSet)
+
+miSet.add("Santy")
+
+console.log(miSet)
+
+miSet.add("Javascript")
+
+console.log(miSet)
+
+
+
+
+
